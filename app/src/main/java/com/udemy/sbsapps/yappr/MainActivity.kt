c1d4aeb4d55abfac4de276981307879c49e956b5
Joe Ortega <joeortega79@gmail.com>
2018-04-24 13:15:41 +0930

    Main UI view is complete and togglebuttons are now toggling

--- app/src/main/java/com/udemy/sbsapps/yappr/MainActivity.kt
+++ app/src/main/java/com/udemy/sbsapps/yappr/MainActivity.kt
@@ -6,10 +6,15 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import kotlinx.android.synthetic.main.activity_add_thought.*
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var selectedCategory = FUNNY
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +25,38 @@ class MainActivity : AppCompatActivity() {
             val addThoughtIntent = Intent(this, AddThoughtActivity::class.java)
             startActivity(addThoughtIntent)
         }
+    }
+
+    fun mainFunnyClicked (view: View) {
+        if(selectedCategory == FUNNY) {
+            mainFunnyButton.isChecked = true
+        }
+        mainSeriousButton.isChecked = false
+        mainCrazyButton.isChecked = false
+        mainPopularButton.isChecked = false
+    }
+    fun mainSeriousClicked (view: View) {
+        if(selectedCategory == SERIOUS) {
+            mainSeriousButton.isChecked = true
+        }
+        mainFunnyButton.isChecked = false
+        mainCrazyButton.isChecked = false
+        mainPopularButton.isChecked = false
+    }
+    fun mainCrazyClicked (view: View) {
+        if(selectedCategory == CRAZY) {
+            mainCrazyButton.isChecked = true
+        }
+        mainFunnyButton.isChecked = false
+        mainSeriousButton.isChecked = false
+        mainPopularButton.isChecked = false
+    }
+    fun mainPopularClicked (view: View) {
+        if(selectedCategory == POPULAR) {
+            mainPopularButton.isChecked = true
+        }
+        mainFunnyButton.isChecked = false
+        mainSeriousButton.isChecked = false
+        mainCrazyButton.isChecked = false
     }
 }
