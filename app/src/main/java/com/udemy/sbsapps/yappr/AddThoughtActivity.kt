451ba13c417ca71c0a995e78f2f535916530d5eb
Joe Ortega <joeortega79@gmail.com>
2018-04-24 12:08:11 +0930

    Thoughts now posts to the Firestore

--- app/src/main/java/com/udemy/sbsapps/yappr/AddThoughtActivity.kt
+++ app/src/main/java/com/udemy/sbsapps/yappr/AddThoughtActivity.kt
@@ -2,17 +2,59 @@ package com.udemy.sbsapps.yappr
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_add_thought.*
 
 class AddThoughtActivity : AppCompatActivity() {
+
+    var selectedCategory = FUNNY
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_thought)
     }
 
-    fun addPostClicked (view: View) {}
-    fun addFunnyClicked (view: View) {}
-    fun addSeriousClicked (view: View) {}
-    fun addCrazyClicked (view: View) {}
+    fun addPostClicked (view: View) {
+        // add post to Firestore
+        val data = HashMap<String, Any>()
+        data.put("category", selectedCategory)
+        data.put("numComments",0)
+        data.put("numLikes",0)
+        data.put("thoughtText",addThoughtText.text.toString())
+        data.put("timestamp", FieldValue.serverTimestamp())
+        data.put("username", addUsernameText.text.toString())
+
+        FirebaseFirestore.getInstance().collection(THOUGHTS_REF)
+                .add(data)
+                .addOnSuccessListener {
+                    finish()
+                }
+                .addOnFailureListener { exception ->
+                    Log.e("Exception", "Could not add post: $exception")
+                }
+    }
+    fun addFunnyClicked (view: View) {
+        if(selectedCategory == FUNNY) {
+            addFunnyButton.isChecked = true
+        }
+        addSeriousButton.isChecked = false
+        addCrazyButton.isChecked = false
+    }
+    fun addSeriousClicked (view: View) {
+        if(selectedCategory == SERIOUS) {
+            addSeriousButton.isChecked = true
+        }
+        addFunnyButton.isChecked = false
+        addCrazyButton.isChecked = false
+    }
+    fun addCrazyClicked (view: View) {
+        if(selectedCategory == CRAZY) {
+            addCrazyButton.isChecked = true
+        }
+        addFunnyButton.isChecked = false
+        addSeriousButton.isChecked = false
+    }
 }
---
+++ app/src/main/java/com/udemy/sbsapps/yappr/Constants.kt
@@ -0,0 +1,13 @@
+package com.udemy.sbsapps.yappr
+
+/**
+ * Created by michaelmallamo on 24/04/2018.
+ */
+
+//Collections Refernece
+const val THOUGHTS_REF = "thoughts"
+
+// Category Constants
+const val FUNNY = "funny"
+const val SERIOUS = "serious"
+const val CRAZY = "crazy"
